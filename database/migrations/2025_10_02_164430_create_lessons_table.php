efd51c1b6a8fa24f17b219290978bf24146da5dc
GuilhermeFreitas2008 <guilherme.filipe.freitas@gmail.com>
2025-10-02 19:15:53 +0100

    Estrutura inicial da BD (Migrations)

---
+++ database/migrations/2025_10_02_164430_create_lessons_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('module_id');
+            $table->string('title', 255);
+            $table->text('content')->nullable();
+            $table->string('video_url', 255)->nullable();
+            $table->integer('position')->default(0);
+            $table->timestamps();
+
+            $table->foreign('module_id')->references('id')->on('modules')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
